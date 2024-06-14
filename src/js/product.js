5559efeee78e2d346942208f84d0df6952d12c91
jacobperry <perryjacob@outlook.com>
2024-06-13 20:54:29 -0700

    Linted and formatted product.js

--- src/js/product.js
+++ src/js/product.js
@@ -4,8 +4,8 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  let currentCart = getLocalStorage("so-cart") || []
-  currentCart.push(product)
+  let currentCart = getLocalStorage("so-cart") || [];
+  currentCart.push(product);
   setLocalStorage("so-cart", currentCart);
 }
 // add to cart button event handler
