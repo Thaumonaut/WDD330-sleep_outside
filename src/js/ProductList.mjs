631c0a2a62d21d0f2e1188e36f1387c4ef612e43
jacobperry <perryjacob@outlook.com>
2024-06-22 12:53:45 -0700

    refactor to dynamically load from json into index

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,30 @@
+import { renderListWithTemplate } from "./utils.mjs"
+
+const productCardTemplate = function(product) {
+  return `<li class="product-card">
+            <a href="product_pages/?product=${product.Id}">
+              <img src="${product.Image}" alt="${product.NameWithoutBrand}" />
+              <h3 class="card__brand">${product.Brand.Name}</h3>
+              <h2 class="card__name">${product.Name}</h2>
+              <p class="product-card__price">${product.ListPrice}</p>
+            </a>
+          </li>`
+}
+
+export default class ProductListing {
+  constructor(category, dataSource, listElement) {
+    this.category = category
+    this.dataSource = dataSource
+    this.listElement = listElement
+  }
+
+  renderList(list) {
+    const filteredList = list.filter((p) => p.Id != "989CG" && p.Id != "880RT")
+    renderListWithTemplate(productCardTemplate, this.listElement, filteredList)
+  }
+
+  async init() {
+    const list = await this.dataSource.getData()
+    this.renderList(list)
+  }
+}
